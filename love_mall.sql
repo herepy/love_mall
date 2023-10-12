a2697d59544746b7dba22c528e3ed6634d35efc3
pengyu <pengyu@cnhsqk.com>
2023-10-12 11:12:33 +0800

    update database sql

--- love_mall.sql
+++ love_mall.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50741
  File Encoding         : 65001
 
- Date: 11/10/2023 18:21:36
+ Date: 12/10/2023 11:11:29
 */
 
 SET NAMES utf8mb4;
@@ -29,9 +29,13 @@ CREATE TABLE `article`  (
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
   `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
   `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `publish_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '发布时间',
   `article_category_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '文章分类',
-  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态，0正常，1作废',
   `draft` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否草稿，1是',
+  `collect_count` smallint(4) UNSIGNED NOT NULL DEFAULT 0 COMMENT '收藏数',
+  `view_count` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '浏览数',
+  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态，0正常，1作废',
+  `sort` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '排序，越小越前',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
@@ -44,6 +48,114 @@ CREATE TABLE `article_category`  (
   `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '分类名',
   `sort` smallint(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT '排序，越小越前',
   `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态，0正常，1作废',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for article_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `article_comment`;
+CREATE TABLE `article_comment`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '文章id',
+  `user_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '评论内容',
+  `parant_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '上级评论id',
+  `root_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '根级评论id',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态，0正常，1作废',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for article_image
+-- ----------------------------
+DROP TABLE IF EXISTS `article_image`;
+CREATE TABLE `article_image`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '文章id',
+  `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '图片url',
+  `sort` tinyint(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '排序，越小越前',
+  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态，0正常，1作废',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for banner
+-- ----------------------------
+DROP TABLE IF EXISTS `banner`;
+CREATE TABLE `banner`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '图片url',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '跳转url',
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '标题',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  `sort` smallint(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT '排序，越小越前',
+  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态，0正常，1作废',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for donation
+-- ----------------------------
+DROP TABLE IF EXISTS `donation`;
+CREATE TABLE `donation`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '捐赠物品名',
+  `donation_category_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '分类id',
+  `user_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
+  `cover_image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '封面图',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '详情内容',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `publish_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '发布时间',
+  `collect_count` smallint(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT '收藏数',
+  `view_count` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '浏览数',
+  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态，0正常，1作废',
+  `draft` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否草稿，1是',
+  `donate_status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '赠送状态，0等待赠送，1已选赠送人，2已发货，3完成赠送',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for donation_category
+-- ----------------------------
+DROP TABLE IF EXISTS `donation_category`;
+CREATE TABLE `donation_category`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '分类名',
+  `sort` smallint(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT '排序，越小越前',
+  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态，0正常，1作废',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for donation_user
+-- ----------------------------
+DROP TABLE IF EXISTS `donation_user`;
+CREATE TABLE `donation_user`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `donation_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '捐赠物id',
+  `user_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '候选用户id',
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '留言内容',
+  `selected` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否被选中，1是',
+  `user_address_id` int(11) UNSIGNED NOT NULL COMMENT '用户收货地址id',
+  `express_number` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '快递单号',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for message
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
+  `target_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '目标id',
+  `type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '类型，表明',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否查收，0未查收，1已查收',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
@@ -78,6 +190,20 @@ CREATE TABLE `user_address`  (
   `district` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '区',
   `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '详细地址',
   `default` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否设为默认，1是',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态，0正常，1作废',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user_collect
+-- ----------------------------
+DROP TABLE IF EXISTS `user_collect`;
+CREATE TABLE `user_collect`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
+  `target_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '目标id',
+  `type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '目标类型，表名',
   `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
   `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态，0正常，1作废',
   PRIMARY KEY (`id`) USING BTREE
