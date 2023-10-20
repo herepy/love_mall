cb4da5369b8d4902b0fae447de0e62eede792f6e
pengyu <pengyu@cnhsqk.com>
2023-10-20 11:09:03 +0800

    update:完善图片上传

--- user/internal/svc/uploadImg.go
+++ user/internal/svc/uploadImg.go
@@ -11,10 +11,21 @@ import (
 	"fmt"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"net/http"
+	"path"
 	"time"
 )
 
 func UploadImg(r *http.Request, accessKey, secret, endpoint, bucketName, key string) (string, error) {
+	file, header, err := r.FormFile(key)
+	if err != nil {
+		return "", err
+	}
+
+	ext := path.Ext(header.Filename)
+	if ext != ".jpg" && ext != ".png" && ext != ".jpeg" && ext != ".bmp" {
+		return "", errors.New("not image file")
+	}
+
 	client, err := oss.New(endpoint, accessKey, secret)
 	if err != nil {
 		return "", err
@@ -25,21 +36,7 @@ func UploadImg(r *http.Request, accessKey, secret, endpoint, bucketName, key str
 		return "", err
 	}
 
-	objectName := fmt.Sprintf("image/%d.png", time.Now().Unix())
-	err = r.ParseMultipartForm(5242880)
-	if err != nil {
-		return "", err
-	}
-
-	fileList, ok := r.MultipartForm.File[key]
-	if !ok {
-		return "", errors.New("upload file not found with key:" + key)
-	}
-
-	file, err := fileList[0].Open()
-	if err != nil {
-		return "", err
-	}
+	objectName := fmt.Sprintf("image/%d%s", time.Now().Unix(), ext)
 
 	defer file.Close()
 	err = bucket.PutObject(objectName, file)
